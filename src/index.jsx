22d7efc946e91a6be4802b27c91ccfcfc7a4451e
hikio-17 <fajri010217@gmail.com>
2023-09-17 13:21:19 +0700

    starter project

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import NotesApp from './components/NotesApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NotesApp />
+  </React.StrictMode>,
+)
